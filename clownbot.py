07f93472eff0aa67a124086b537f8d153d8be015
Richard Hartnell <109207464+richard-hartnell@users.noreply.github.com>
2024-06-20 22:35:22 -0700

    New subs added

--- clownbot.py
+++ clownbot.py
@@ -18,6 +18,8 @@ exempt_terms = ["class clown",
                 ]
 subs = ['funny',
         'internationalnews',
+        'aboringdystopia',
+        'lostgeneration',
         'todayilearned',
         'music',
         'movies',
